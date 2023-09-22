06c5cb9fb032588f8b820831674f146928da05f7
Fisch-dp <cychandp@connect.ust.hk>
2023-09-22 09:36:18 +0800

    Inverted cases will have 0.5 cancer and 0.5 invasive

--- Dataset.py
+++ Dataset.py
@@ -67,18 +67,16 @@ class CustomDataset(Dataset):
         if sample.difficult_negative_case == 1 and sample.biopsy == 1 and self.Train and random.random() < cfg.invert_difficult:
             mask = self.df.query(f'cancer == 1 & implant == {sample.implant} & site_id == {sample.site_id} & view == {sample["view"]}')
             sample = self.df.iloc[np.random.choice(mask.index)]
-            data['cancer'] = np.expand_dims(np.array(sample.cancer, dtype=np.int8), axis=0)
-            data['invasive'] = np.expand_dims(np.array(sample.invasive, dtype=np.int8), axis=0)
-            image_data = {"image": os.path.join(self.cfg.root_dir, f"{sample.patient_id}_{sample.image_id}.png")}
-            image_data = self.aug(image_data)
+            data['cancer'] = (data['cancer'] + np.expand_dims(np.array(sample.cancer, dtype=np.int8), axis=0)) / 2
+            data['invasive'] = (data['invasive'] + np.expand_dims(np.array(sample.invasive, dtype=np.int8), axis=0)) / 2
+            image_data = self.aug({"image": os.path.join(self.cfg.root_dir, f"{sample.patient_id}_{sample.image_id}.png")})
             if (cfg.Trans is not None and self.Train):
                 image_data['image'] = image_data['image'].permute(1,2,0) * 255
                 image_data["image"] = cfg.Trans(image=np.array(image_data['image'].to(torch.uint8)))['image']
                 Trans2 = ToTensorV2(transpose_mask=False, always_apply=True, p=1.0)
                 image_data['image'] = Trans2(image=image_data['image'])['image']
                 image_data['image'] = image_data['image'].to(torch.float32) / 255
-            data['image'] += image_data['image']
-            data['image'] /= 2
+            data['image'] = (data['image'] + image_data['image']) / 2
 
         return data
 

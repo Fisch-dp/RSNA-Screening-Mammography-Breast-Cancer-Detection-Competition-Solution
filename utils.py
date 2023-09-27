8b374a9d0e07c00c2af20222fa0edfab310ebf4a
Fisch-dp <cychandp@connect.ust.hk>
2023-09-27 12:31:32 +0800

    debug

--- utils.py
+++ utils.py
@@ -55,8 +55,8 @@ def triplet_loss(y_pred, prediction_id_list, margin=0):
         for i in loss: i.to(cfg.device)
         margin = torch.tensor(margin).to(cfg.device)
         for prediction_id in prediction_id_list:
-            pos_indices = [index for index, element in enumerate(prediction_id_list) if element == prediction_id]
-            neg_indices = [index for index, element in enumerate(prediction_id_list) if element != prediction_id]
+            pos_indices = torch.tensor([index for index, element in enumerate(prediction_id_list) if element == prediction_id]).to(cfg.device)
+            neg_indices = torch.tensor([index for index, element in enumerate(prediction_id_list) if element != prediction_id]).to(cfg.device)
             loss[0] += torch.norm(y_pred[pos_indices].unsqueeze(1) - y_pred[pos_indices].unsqueeze(0), dim=2).mean()
             loss[1] += torch.norm(y_pred[pos_indices].unsqueeze(1) - y_pred[neg_indices].unsqueeze(0), dim=2).mean()
             loss[2] += torch.max(loss[0] - loss[1] + margin, torch.tensor(0).to(cfg.device))#only hard triplets

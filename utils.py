8a69ff0cabb2b7e3a44f58b67d121adf4723bc30
Fisch-dp <cychandp@connect.ust.hk>
2023-09-27 11:03:33 +0800

    debug

--- utils.py
+++ utils.py
@@ -51,14 +51,12 @@ class MultiImageBatchSampler(torch.utils.data.Sampler):
         return len(self.df)
     
 def triplet_loss(y_pred, prediction_id_list, margin=0):
-        series1 = pd.Series(id)
         loss =[0, 0, 0]# [positive, negative, triplet]
         for prediction_id in prediction_id_list:
-            pos_indices = series1[series1 == prediction_id].index
-            neg_indices = series1[series1 != prediction_id].index
+            pos_indices = np.where(prediction_id_list == prediction_id)
+            neg_indices = np.where(prediction_id_list != prediction_id)
             loss[0] += F.pairwise_distance(y_pred[pos_indices].unsqueeze(1), y_pred[pos_indices].unsqueeze(0), p=2).mean()
             loss[1] += F.pairwise_distance(y_pred[pos_indices].unsqueeze(1), y_pred[neg_indices].unsqueeze(0), p=2).mean()
-            print(loss)
             loss[2] += loss[0] - loss[1] #loss[2] = (loss[0] - loss[1], margin).max()
         return loss[2]
 

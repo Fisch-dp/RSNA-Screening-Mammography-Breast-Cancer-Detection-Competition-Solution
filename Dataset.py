3d102c6d3fd70231d00381729fd3a7f6edabcead
Fisch-dp <cychandp@connect.ust.hk>
2023-10-01 16:24:00 +0800

    debug

--- Dataset.py
+++ Dataset.py
@@ -47,15 +47,15 @@ class CustomDataset(Dataset):
         if self.Train and random.random() < cfg.invert_difficult:
             if sample.difficult_negative_case == 1 and sample.biopsy == 1:
                 mask = self.df.query(f'cancer == 1')
-            sample = self.df.iloc[np.random.choice(mask.index)]
-            supp_data = read(sample, self.aug, self.cfg, self.Train)
-            if self.mixFunction == "simple":
-                data = simple_invert(data, supp_data, self.cfg)
-            elif self.mixFunction == "Mixup":
-                data = Mixup(data, supp_data, force_label = self.cfg.force_label)
-            elif self.mixFunction == "CutMix":
-                data = CutMix(data, supp_data, force_label = self.cfg.force_label)
-            
+                sample = self.df.iloc[np.random.choice(mask.index)]
+                supp_data = read(sample, self.aug, self.cfg, self.Train)
+                if self.mixFunction == "simple":
+                    data = simple_invert(data, supp_data, self.cfg)
+                elif self.mixFunction == "Mixup":
+                    data = Mixup(data, supp_data, force_label = self.cfg.force_label)
+                elif self.mixFunction == "CutMix":
+                    data = CutMix(data, supp_data, force_label = self.cfg.force_label)
+                
         return data
 
     def __len__(self):
